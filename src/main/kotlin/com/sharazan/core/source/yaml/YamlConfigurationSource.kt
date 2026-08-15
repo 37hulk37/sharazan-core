a9dfde7e0687058b7391bd750070b9c9cddc2ae4
Alexander Ushakov <60547863+37hulk37@users.noreply.github.com>
2026-08-15 23:21:24 +0300

    docker file

--- src/main/kotlin/com/sharazan/core/source/yaml/YamlConfigurationSource.kt
+++ src/main/kotlin/com/sharazan/core/source/yaml/YamlConfigurationSource.kt
@@ -1,7 +1,9 @@
 package com.sharazan.core.source.yaml
 
 import com.charleskorn.kaml.Yaml
+import com.charleskorn.kaml.YamlConfiguration
 import com.charleskorn.kaml.YamlMap
+import com.charleskorn.kaml.YamlNamingStrategy
 import com.charleskorn.kaml.YamlNode
 import com.sharazan.core.source.ConfigurationSource
 import kotlinx.serialization.DeserializationStrategy
@@ -10,12 +12,14 @@ class YamlConfigurationSource(
     private val root: YamlNode
 ) : ConfigurationSource {
 
+    private val yaml = Yaml(configuration = YamlConfiguration(yamlNamingStrategy = YamlNamingStrategy.KebabCase))
+
     override fun <T : Any> get(prefix: String, deserializer: DeserializationStrategy<T>): T {
         val scoped = prefix.split(".").fold(root) { node, key ->
             (node as YamlMap).get<YamlNode>(key) ?: error("Missing YAML key: $prefix")
         }
 
-        return Yaml.default.decodeFromYamlNode(deserializer, scoped)
+        return yaml.decodeFromYamlNode(deserializer, scoped)
     }
 
 }
